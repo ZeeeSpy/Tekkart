9125396b1b5ec55b56eab4df8766e492fd049edc
IliaBahmutov <52408039+IliaBahmutov@users.noreply.github.com>
2019-12-27 18:39:11 +0000

    CheckPoint System

---
+++ Tekkart/Assets/CheckPointScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointScript : MonoBehaviour
+{
+    private LapNumber LapParent;
+    private int position = -1;
+    private KartScript ThisKart;
+
+    public void SetUpPosition(int masterposition, LapNumber IncLapParent)
+    {
+        position = masterposition;
+        LapParent = IncLapParent;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ThisKart = other.gameObject.GetComponent<KartScript>();
+
+
+        if (ThisKart != null)
+        {
+            LapParent.CheckIn(position);
+        }
+    }
+}
---
+++ Tekkart/Assets/LapNumber.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LapNumber : MonoBehaviour
+{
+    public Text LapNumberUI; 
+
+    private int TargetCheckpoint = 0;
+    private int NumberOfCheckpoints = -1;
+    private bool newlap = false;
+    private int lapnumber = 1;
+
+    private void Awake()
+    {
+        CheckPointScript[] CheckpointArray = GetComponentsInChildren<CheckPointScript>();
+        NumberOfCheckpoints = CheckpointArray.Length;
+        int i = 0;
+        foreach (CheckPointScript Checkpoint in CheckpointArray)
+        {
+            Checkpoint.SetUpPosition(i, this);
+            i++;
+        }
+    }
+
+    public void CheckIn(int inccheckpoint)
+    {
+        if (inccheckpoint == TargetCheckpoint)
+        {
+            TargetCheckpoint++;
+
+            if (newlap)
+            {
+                newlap = false;
+                lapnumber++;
+                LapNumberUI.text = "Lap " + lapnumber.ToString() + "/∞";
+            }
+
+            if (TargetCheckpoint == NumberOfCheckpoints)
+            {
+                TargetCheckpoint = 0;
+                newlap = true;
+            }
+        }
+    }
+}
