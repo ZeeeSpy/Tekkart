fb790cf1bd21a4f207fc69279fbb61463362f16b
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-01-04 15:19:01 +0000

    Pick Up Crate Stuff

---
+++ Tekkart/Assets/Scripts/PickUpCrate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCrate : MonoBehaviour
+{
+    private MeshRenderer MR;
+    private BoxCollider BC;
+
+    private void Awake()
+    {
+        MR = GetComponent<MeshRenderer>();
+        BC = GetComponent<BoxCollider>();
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Kart ThisKart = other.gameObject.GetComponent<Kart>();
+
+        if (ThisKart != null)
+        {
+            StartCoroutine("Reset");
+        }
+    }
+
+    IEnumerator Reset()
+    {
+        MR.enabled = false;
+        BC.enabled = false;
+        yield return new WaitForSeconds(5f);
+        MR.enabled = true;
+        BC.enabled = true;
+    }
+}
