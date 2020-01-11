2086635068f28fbbb09640705104a62f207527cf
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-01-11 19:43:31 +0000

    Ai item logic start

---
+++ Tekkart/Assets/AIPickup.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPickup : MonoBehaviour, Pickupable
+{
+    private bool HasPickUp = false;
+    private int ItemNumb = -1;
+    private KartScript ThisKart;
+    private ItemParent ItemList;
+    public GameObject Normal;
+    private bool CoroutineRunning = false;
+
+    private void Awake()
+    {
+        ThisKart = GetComponent<KartScript>();
+        ItemList = GameObject.FindGameObjectWithTag("ItemParent").GetComponent<ItemParent>();
+    }
+
+    public void GetPickUp()
+    {
+        if (!HasPickUp)
+        {
+            int numb = Random.Range(0, 3);
+            ItemNumb = numb;
+            HasPickUp = true;
+            Debug.Log("Got Item:" + numb);
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 forward = Normal.transform.TransformDirection(Vector3.forward) * 10 + new Vector3(0,2,0);
+        Debug.DrawRay(Normal.transform.position + new Vector3(0, 2, 0), forward, Color.green);
+        
+
+        if (HasPickUp && !CoroutineRunning)
+        {
+            switch (ItemNumb)
+            {
+                case 0:
+                    ThisKart.Boost();
+                    HasPickUp = false;
+                    break;
+                case 1:
+                    StartCoroutine("TrapCoroutine");
+                    break;
+                case 2:
+
+                    /*
+                    Ray ray = new Ray(transform.position, transform.forward);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit, 30f))
+                    {
+                        if (hit.transform.tag == "Characters")
+                        {
+                            //Has LOS
+                            Vector3 rot = Normal.transform.eulerAngles;
+                            rot = new Vector3(rot.x * -1, rot.y + 180, rot.z);
+                            var MissileRot = Quaternion.Euler(rot);
+                            Instantiate(ItemList.GetUnguidedMissile(), (transform.position + transform.forward * 2f), MissileRot);
+                        }
+                    }
+                    */
+
+                    break;
+            }
+        }
+    }
+
+    IEnumerator TrapCoroutine()
+    {
+        CoroutineRunning = true;
+        yield return new WaitForSeconds(Random.Range(5, 16));
+        Instantiate(ItemList.GetTrap(), (transform.position - transform.forward * 2f), Normal.transform.rotation);
+        CoroutineRunning = true;
+        HasPickUp = false;
+    }
+}
+
+
--- Tekkart/Assets/Scripts/PlayerPickup.cs
+++ Tekkart/Assets/Scripts/PlayerPickup.cs
@@ -13,7 +13,7 @@ public class PlayerPickup : MonoBehaviour, Pickupable
 
     private void Awake()
     {
-        ItemArray = new string[4] { "Boost", "Airstrike", "Trap", "UnguidedMissile" };
+        ItemArray = new string[3] { "Boost", "Trap", "UnguidedMissile" };
         ThisKart = GetComponent<KartScript>();
         ItemList = GameObject.FindGameObjectWithTag("ItemParent").GetComponent<ItemParent>();
     }
@@ -22,7 +22,7 @@ public class PlayerPickup : MonoBehaviour, Pickupable
     {
         if (!HasPickUp)
         {
-            int numb = Random.Range(0, 4);
+            int numb = Random.Range(0, 3);
             
             Debug.Log("Got: " + ItemArray[numb]);
             //TODO update ui 
@@ -57,16 +57,13 @@ public class PlayerPickup : MonoBehaviour, Pickupable
                     ThisKart.Boost();
                     break;
                 case 1:
-                    CallInAirStrike();
+                    Instantiate(ItemList.GetTrap(), (transform.position - transform.forward * 2f), Normal.transform.rotation);
                     break;
                 case 2:
-                    Instantiate(ItemList.GetTrap(), transform.position + new Vector3(0, 2, -2.5f), Normal.transform.rotation);
-                    break;
-                case 3:
                     Vector3 rot = Normal.transform.eulerAngles;
                     rot = new Vector3(rot.x *-1, rot.y + 180, rot.z);
                     var MissileRot = Quaternion.Euler(rot);
-                    Instantiate(ItemList.GetUnguidedMissile(), transform.position + new Vector3(0, 1, 2), MissileRot);
+                    Instantiate(ItemList.GetUnguidedMissile(), (transform.position + transform.forward * 2f), MissileRot);
                     break;
             }
         }
@@ -75,10 +72,7 @@ public class PlayerPickup : MonoBehaviour, Pickupable
     private void CallInAirStrike()
     {
         /*
-         * attack only first place
-         * 
-         * 
-         * 
+         * attack only first place/Select who to target
          */
     }
 }
