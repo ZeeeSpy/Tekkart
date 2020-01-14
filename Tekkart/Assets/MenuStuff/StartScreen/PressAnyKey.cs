c9aaade9332a9a40961f469bdc6a93c3567fcaa0
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-01-14 21:55:48 +0000

    PressAnyKeyScript

---
+++ Tekkart/Assets/MenuStuff/StartScreen/PressAnyKey.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAnyKey : MonoBehaviour
+{
+    public GameObject toenable;
+    public GameObject todisable;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            toenable.SetActive(true);
+            todisable.SetActive(false);
+        }
+    }
+}
