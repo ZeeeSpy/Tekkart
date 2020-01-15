83d18c000855678213d4644f41ffa474086cc3ea
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-01-15 15:54:06 +0000

    menu sfx

---
+++ Tekkart/Assets/MenuStuff/MainMenu/MainMenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public AudioSource SFX;
+    public AudioClip Highlight;
+    public AudioClip Select;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonHover()
+    {
+        SFX.PlayOneShot(Highlight);
+    }
+
+    public void OnButtonClick()
+    {
+        SFX.PlayOneShot(Select);
+    }
+}
--- Tekkart/Assets/MenuStuff/StartScreen/PressAnyKey.cs
+++ Tekkart/Assets/MenuStuff/StartScreen/PressAnyKey.cs
@@ -7,10 +7,17 @@ public class PressAnyKey : MonoBehaviour
     public GameObject toenable;
     public GameObject todisable;
     // Update is called once per frame
+    public AudioSource SFX;
+    public AudioSource OST;
+
+    public AudioClip SFXSelect;
+    
     void Update()
     {
         if (Input.anyKey)
         {
+            SFX.PlayOneShot(SFXSelect);
+            OST.Play();
             toenable.SetActive(true);
             todisable.SetActive(false);
         }
