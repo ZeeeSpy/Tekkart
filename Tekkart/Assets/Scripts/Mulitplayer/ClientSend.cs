0c4b3a31127e9152d2dc2e6fe2ce10cf58118d8c
Ilia <52408039+IliaBahmutov@users.noreply.github.com>
2020-02-15 17:45:03 +0000

    Sending and Receiving Data Using TCP

---
+++ Tekkart/Assets/Scripts/Mulitplayer/ClientSend.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSend : MonoBehaviour
+{
+    private static void SendTCPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.instance.tcp.SendData(_packet);
+    }
+
+    public static void WelcomeReceived()
+    {
+        using (Packet _packet = new Packet((int)ClientPackets.welcomeReceived))
+        {
+            _packet.Write(Client.instance.myId);
+            _packet.Write(PlayerPrefs.GetString("PLAYER_NAME"));
+
+            SendTCPData(_packet);
+        }
+    }
+}
